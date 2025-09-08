91d7c05748def0ebaef5baf5548201b864ae344b
Nicholas-Cheng-De-Fei <nicholaschenguu@gmail.com>
2025-09-09 02:33:17 +0800

    Change to DaisyUI

---
+++ PeerPrep/src/frontend/pages/App.tsx
@@ -0,0 +1,12 @@
+import LandingPage from "./LandingPage"
+
+function App() {
+    return (
+        /* Set our background */
+        <div className="bg-neutral-100 w-screen h-screen p-5">
+            <LandingPage />
+        </div>
+    )
+}
+
+export default App
