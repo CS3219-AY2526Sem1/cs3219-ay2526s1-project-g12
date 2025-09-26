727da8a840298ef42b35c9f3485e1a28117070cc
Jovin Ang <36396502+Jovin-Ang@users.noreply.github.com>
2025-09-26 23:03:50 +0800

    Update user schemas to enforce password length and refine role_id validation

--- user_svc/auth/schemas.py
+++ user_svc/auth/schemas.py
@@ -8,16 +8,17 @@ from pydantic import Field
 class UserRead(schemas.BaseUser[uuid.UUID]):
     first_name: Annotated[str, Field(min_length=1, max_length=50)]
     last_name: Annotated[str, Field(min_length=1, max_length=50)]
-    role_id: Annotated[int, Field(gt=0, decimal_places=0)]
+    role_id: Annotated[int, Field(gt=0)]
 
 
 class UserCreate(schemas.BaseUserCreate):
+    password: Annotated[str, Field(min_length=8)]
     first_name: Annotated[str, Field(min_length=1, max_length=50)]
     last_name: Annotated[str, Field(min_length=1, max_length=50)]
-    role_id: Annotated[int, Field(gt=0, decimal_places=0)]
+    role_id: Annotated[int, Field(gt=0)]
 
 
 class UserUpdate(schemas.BaseUserUpdate):
     first_name: Annotated[str | None, Field(min_length=1, max_length=50)] = None
     last_name: Annotated[str | None, Field(min_length=1, max_length=50)] = None
-    role_id: Annotated[int | None, Field(gt=0, decimal_places=0)] = None
+    role_id: Annotated[int | None, Field(gt=0)] = None
