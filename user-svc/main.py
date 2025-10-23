a3b0f11f15fc2841003075bed7593c2b7e411e53
Dong Qianbo <pencilate@outlook.com>
2025-10-24 00:20:31 +0800

    Revert "Add service host configuration to environment and FastAPI setup"
    This reverts commit 032db7ec76a7819393c6efe8e1df91c3075c6a6e.

--- user-svc/main.py
+++ user-svc/main.py
@@ -2,11 +2,8 @@ from fastapi import FastAPI
 
 from routes.auth_router import router as auth_router
 from routes.user_router import router as user_router
-from utils.utils import AppConfig
 
-config = AppConfig()
-
-app = FastAPI(title="PeerPrep User Service", servers=[{"url": config.service_host}])
+app = FastAPI(title="PeerPrep User Service")
 
 app.include_router(auth_router)
 app.include_router(user_router)
