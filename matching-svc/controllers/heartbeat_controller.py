a345079625a609728f6e898de9c803cfb095e9b1
sheenkerr <46549752+sheenkerr@users.noreply.github.com>
2025-10-24 01:33:09 +0800

    Update matching-svc to register routes with gateway on startup & send heartbeat

---
+++ matching-svc/controllers/heartbeat_controller.py
@@ -0,0 +1,66 @@
+
+import asyncio
+from uuid import uuid4
+
+import requests
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from requests.exceptions import RequestException
+
+from utils.logger import log
+from utils.utils import get_envvar
+
+SERVICE_NAME = "matching-svc"
+INSTANCE_ID = str(uuid4())
+
+
+def register_self_as_service(app: FastAPI):
+    log.info(f"Registering service with {SERVICE_NAME} {INSTANCE_ID}")
+    openapi_schema = get_openapi(
+        title=f"{SERVICE_NAME} API",
+        version="1.0.0",
+        routes=app.routes,
+    )
+
+    log.debug(openapi_schema)
+
+    json_payload = {
+        "service_name": SERVICE_NAME,
+        "instance_id": INSTANCE_ID,
+        "address": get_envvar("HOST_URL"),
+        "openapi": openapi_schema,
+    }
+
+    try:
+        requests.post(
+            f"{get_envvar('APIGATEWAY_URL')}{get_envvar('REGISTRY_PATH')}",
+            json=json_payload,
+        )
+    except RequestException as e:
+        log.debug(e)
+        log.warning("Could not register on API Gateway")
+
+
+def _send_healthcheck():
+    log.info("Sending Healthcheck now")
+    json_payload = {"service_name": SERVICE_NAME, "instance_id": INSTANCE_ID}
+    try:
+        requests.post(
+            f"{get_envvar('APIGATEWAY_URL')}{get_envvar('HEARTBEAT_PATH')}",
+            json=json_payload,
+        )
+    except RequestException as e:
+        log.debug(e)
+        log.warning("Could not register on API Gateway")
+
+
+async def _periodic_healtcheck():  # pragma: no cover
+    while True:
+        _send_healthcheck()
+        await asyncio.sleep(int(get_envvar("HEARTBEAT_PERIOD")))
+
+
+def register_heartbeat():  # pragma: no cover
+    log.info("Registering heartbeat")
+    loop = asyncio.get_event_loop()
+    return loop.create_task(_periodic_healtcheck())
--- matching-svc/routes.py
+++ matching-svc/routes.py
@@ -1,5 +1,10 @@
 from contextlib import asynccontextmanager
-from controllers.matching_controller import find_match, check_redis_connection, confirm_match, terminate_match
+from controllers.matching_controller import (
+    find_match,
+    check_redis_connection,
+    confirm_match,
+    terminate_match,
+)
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from models.api_models import MatchRequest, MatchConfirmRequest
@@ -7,7 +12,19 @@ from service.redis_confirmation_service import connect_to_redis_confirmation_ser
 from service.redis_message_service import connect_to_redis_message_service
 from service.redis_matchmaking_service import connect_to_redis_matchmaking_service
 from utils.logger import log
-from utils.utils import sever_connection
+from utils.utils import sever_connection, get_envvar
+
+from controllers.heartbeat_controller import (
+    register_heartbeat,
+    register_self_as_service,
+)
+
+HOST_URL = get_envvar("HOST_URL")
+FRONT_END_URL = get_envvar("FRONT_END_URL")
+
+ADMIN_ROLE = "admin"
+USER_ROLE = "user"
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -18,42 +35,75 @@ async def lifespan(app: FastAPI):
     app.state.redis_message_service = connect_to_redis_message_service()
     app.state.redis_confirmation_service = connect_to_redis_confirmation_service()
     log.info("Matching service is Up.")
+    register_self_as_service(app)
+    hc_task = register_heartbeat()
     yield
     # This is the shut down procedure when the matching service stops.
-    log.info("Matching service shutting down.")
+    log.info(
+        "Matching service shutting down.",
+        openapi_extra={"x-roles": [ADMIN_ROLE, USER_ROLE]},
+    )
     await sever_connection(app.state.redis_matchmaking_service)
     await sever_connection(app.state.redis_message_service)
     await sever_connection(app.state.redis_confirmation_service)
-    
-app = FastAPI(lifespan=lifespan)
+    hc_task.cancel()
+
+
+app = FastAPI(title="PeerPrep Matching Service", lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[FRONT_END_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def root():
     return {"status": "success"}
 
-@app.get("/check_connection/queue")
+
+@app.get("/check_connection/queue", openapi_extra={"x-roles": [ADMIN_ROLE, USER_ROLE]})
 async def check_queue_connection():
     return check_redis_connection(app.state.redis_queue)
 
-@app.get("/check_connection/message_queue")
+
+@app.get(
+    "/check_connection/message_queue",
+    openapi_extra={"x-roles": [ADMIN_ROLE, USER_ROLE]},
+)
 async def check_message_connection():
     return check_redis_connection(app.state.redis_message_queue)
 
-@app.post("/find_match")
+
+@app.post("/find_match", openapi_extra={"x-roles": [ADMIN_ROLE, USER_ROLE]})
 async def match(match_request: MatchRequest):
-    return  await find_match(match_request, app.state.redis_matchmaking_service, app.state.redis_message_service, app.state.redis_confirmation_service)
+    return await find_match(
+        match_request,
+        app.state.redis_matchmaking_service,
+        app.state.redis_message_service,
+        app.state.redis_confirmation_service,
+    )
 
-@app.delete("/terminate_match")
+
+@app.delete("/terminate_match", openapi_extra={"x-roles": [ADMIN_ROLE, USER_ROLE]})
 async def terminate(cancel_request: MatchRequest):
-    return await terminate_match(cancel_request, app.state.redis_matchmaking_service, app.state.redis_message_service)
+    return await terminate_match(
+        cancel_request,
+        app.state.redis_matchmaking_service,
+        app.state.redis_message_service,
+    )
 
-@app.post("/confirm_match/{match_id}")
+
+@app.post(
+    "/confirm_match/{match_id}", openapi_extra={"x-roles": [ADMIN_ROLE, USER_ROLE]}
+)
 async def confirm_user_match(match_id: str, confirm_request: MatchConfirmRequest):
-    return await confirm_match(match_id, confirm_request, app.state.redis_matchmaking_service, app.state.redis_message_service,  app.state.redis_confirmation_service)
+    return await confirm_match(
+        match_id,
+        confirm_request,
+        app.state.redis_matchmaking_service,
+        app.state.redis_message_service,
+        app.state.redis_confirmation_service,
+    )
