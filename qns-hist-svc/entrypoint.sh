bd8d46a217ce62f6c3600e459d08ee26a53fc57c
Dong Qianbo <pencilate@outlook.com>
2025-10-27 02:08:23 +0800

    Add svc to docker compose

--- qns-hist-svc/entrypoint.sh
+++ qns-hist-svc/entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-/app/.venv/bin/aerich upgrade && exec /app/.venv/bin/fastapi run /app/routes.py $@
+/app/.venv/bin/aerich upgrade && /app/.venv/bin/celery --app service.feedback_ai_svc worker -c 1 --loglevel=INFO & exec /app/.venv/bin/fastapi run /app/routes.py $@
+wait -n
+exit $?
