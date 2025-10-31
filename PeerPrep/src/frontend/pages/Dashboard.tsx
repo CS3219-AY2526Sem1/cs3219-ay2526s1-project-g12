28d561691b2b1c58d1dd9a7ed2553b293903bb15
Dong Qianbo <pencilate@outlook.com>
2025-10-31 15:44:25 +0800

    Add attempt to dashboard

--- PeerPrep/src/frontend/pages/Dashboard.tsx
+++ PeerPrep/src/frontend/pages/Dashboard.tsx
@@ -1,9 +1,48 @@
+import { useEffect, useState } from 'react';
 import NavBar from '../components/NavBar';
 import { NAV_BUTTONS } from '../config/NavConfig';
 import { useAuth } from '../context/AuthContext';
+import {
+  questionHistoryApi,
+  type QuestionHistory,
+} from '../api/QuestionHistoryApi';
 
 function Dashboard() {
   const { user } = useAuth();
+  const [userAttemptHistory, setUserAttemptHistory] = useState<
+    QuestionHistory[]
+  >([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!user?.id) return;
+
+    const fetchAttempts = async () => {
+      setLoading(true);
+      setError(null);
+      const res =
+        await questionHistoryApi.get_question_history_details_by_user_id();
+      if (res.error) {
+        setError('Failed to load user attempts.');
+      } else if (res.data) {
+        setUserAttemptHistory(res.data);
+      }
+      setLoading(false);
+    };
+
+    fetchAttempts();
+  }, []);
+
+  const summariseTime = (uah: QuestionHistory[]) => {
+    const totalTime: number = uah
+      .map((attempt) => attempt.time_elapsed)
+      .reduce((a, b) => a + b, 0);
+    const hours = totalTime / 60 / 60;
+    const minutes = (totalTime / 60) % 60;
+    return `${hours.toFixed(0)}h ${minutes.toFixed(0)}m`;
+  };
+
   return (
     <div className="min-h-screen px-20 py-10">
       <NavBar buttons={NAV_BUTTONS} />
@@ -13,22 +52,66 @@ function Dashboard() {
         Welcome, {user?.first_name} {user?.last_name}
       </h1>
       <p className="text-2xl p-2 font-light tracking-widest">For your review</p>
-      <div className="flex p-2 gap-6">
-        <div className="card w-72 card-xs shadow-sm border-1 border-base-200">
-          <div className="card-body p-4 gap-0.5">
-            <h2 className="font-medium text-base">Total Interviews Done</h2>
-            <h2 className="font-extrabold text-4xl">40</h2>
-            <p className="font-medium">013 Easy | 020 Medium | 007 Hard</p>
-          </div>
-        </div>
 
-        <div className="card w-72 card-xs shadow-sm border-1 border-base-200">
-          <div className="card-body p-4 gap-0.5">
-            <h2 className="font-medium text-base">Total Time Spent</h2>
-            <h2 className="font-extrabold text-4xl">4h 12m</h2>
+      {loading ? (
+        <div className="flex p-2 gap-6">
+          <div className="card w-72 card-xs shadow-sm border-1 border-base-200">
+            <div className="card-body p-4 gap-0.5">
+              <h2 className="font-extrabold text-4xl">Loading...</h2>
+            </div>
           </div>
         </div>
-      </div>
+      ) : error ? (
+        <div className="flex p-2 gap-6">
+          <div className="card w-72 card-xs shadow-sm border-1 border-base-200">
+            <div className="card-body p-4 gap-0.5">
+              <h2 className="font-extrabold text-4xl">
+                Unable to access your attempt history currently
+              </h2>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="flex p-2 gap-6">
+          <div className="card w-72 card-xs shadow-sm border-1 border-base-200">
+            <div className="card-body p-4 gap-0.5">
+              <h2 className="font-medium text-base">Total Interviews Done</h2>
+              <h2 className="font-extrabold text-4xl">
+                {userAttemptHistory.length}
+              </h2>
+              <p className="font-medium">
+                {
+                  userAttemptHistory.filter(
+                    (attempt) => attempt.difficulty === 'Easy'
+                  ).length
+                }{' '}
+                Easy |{' '}
+                {
+                  userAttemptHistory.filter(
+                    (attempt) => attempt.difficulty === 'Medium'
+                  ).length
+                }{' '}
+                Medium |{' '}
+                {
+                  userAttemptHistory.filter(
+                    (attempt) => attempt.difficulty === 'Hard'
+                  ).length
+                }{' '}
+                Hard
+              </p>
+            </div>
+          </div>
+
+          <div className="card w-72 card-xs shadow-sm border-1 border-base-200">
+            <div className="card-body p-4 gap-0.5">
+              <h2 className="font-medium text-base">Total Time Spent</h2>
+              <h2 className="font-extrabold text-4xl">
+                {summariseTime(userAttemptHistory)}
+              </h2>
+            </div>
+          </div>
+        </div>
+      )}
 
       {/* Recent Matches Table */}
       <p className="text-2xl p-2 font-light tracking-widest">
@@ -40,32 +123,36 @@ function Dashboard() {
           <thead>
             <tr>
               <th></th>
-              <th>Question</th>
               <th>Category</th>
               <th>Difficulty</th>
+              <th>Title</th>
+              <th>Description</th>
               <th>Time Elapsed</th>
-              <th>Partner</th>
+              <th>Attempted At</th>
+              <th>Submitted Solution</th>
+              <th>Sample Solution</th>
+              <th>Feedback</th>
             </tr>
           </thead>
 
           {/* Table Body */}
           <tbody>
-            <tr>
-              <td className="font-semibold">1</td>
-              <td className="font-semibold">Two Sum</td>
-              <td className="font-semibold">Array</td>
-              <td className="font-semibold">Easy</td>
-              <td>30min</td>
-              <td>Bryce Ang Hong Jun</td>
-            </tr>
-            <tr>
-              <td className="font-semibold">2</td>
-              <td className="font-semibold">3 Sum</td>
-              <td className="font-semibold">Array</td>
-              <td className="font-semibold">Medium</td>
-              <td>45min</td>
-              <td>Bryce Ang Hong Jun</td>
-            </tr>
+            {userAttemptHistory.map((e, i) => (
+              <tr>
+                <td className="font-semibold">{i + 1}</td>
+                <td className="font-semibold">{e.category}</td>
+                <td className="font-semibold">{e.difficulty}</td>
+                <td className="font-semibold">{e.title}</td>
+                <td className="font-semibold">{e.description}</td>
+                <td className="font-semibold">{e.time_elapsed}</td>
+                <td className="font-semibold">
+                  {e.attempted_at.toLocaleString()}
+                </td>
+                <td className="font-semibold">{e.submitted_solution}</td>
+                <td className="font-semibold">{e.solution_sample}</td>
+                <td className="font-semibold">{e.feedback}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
