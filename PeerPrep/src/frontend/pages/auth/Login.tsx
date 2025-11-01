cb4fb9e9455802cab12308dc810e92b330f7ead8
Jovin Ang <36396502+Jovin-Ang@users.noreply.github.com>
2025-11-02 03:59:36 +0800

    Fix comparison operator for login page

--- PeerPrep/src/frontend/pages/auth/Login.tsx
+++ PeerPrep/src/frontend/pages/auth/Login.tsx
@@ -72,7 +72,7 @@ export default function Login() {
                 d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
-            {error == 'LOGIN_USER_NOT_VERIFIED' ? (
+            {error === 'LOGIN_USER_NOT_VERIFIED' ? (
               <>
                 <span>Your email has not been verified.</span>
                 <Link
