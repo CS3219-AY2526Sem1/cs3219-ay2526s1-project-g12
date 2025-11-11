d170dee7e08bdd59b16eb27178773095c349e9f5
sheenkerr <46549752+sheenkerr@users.noreply.github.com>
2025-11-11 22:52:55 +0800

    Update cookie retrieval in fe

--- PeerPrep/src/frontend/pages/CollabEditor.tsx
+++ PeerPrep/src/frontend/pages/CollabEditor.tsx
@@ -12,6 +12,7 @@ function getCookie(name: string): string | null {
   }
   const nameEQ = name + '=';
   const ca = document.cookie.split(';');
+  console.log('Cookies:', document.cookie);
   for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
     while (c.charAt(0) === ' ') c = c.substring(1, c.length);
